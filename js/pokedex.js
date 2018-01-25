601358a6516fb02703c662df714d44f7d555a629
InesAtm <ineskatam@gmail.com>
2018-01-25 21:10:46 +0100

    first test

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,8 +1,13 @@
 var pokeApp = angular.module('pokedex', ['ngResource']);
-
+pokeApp.controller('searchPk', ['$scope', 
+function($scope) {
+    $scope.pokemon = {id: '', name: ''};
+    
+  }]);;
 // With this you can inject POKEAPI url wherever you want
 pokeApp.constant('POKEAPI', 'http://pokeapi.co');
 
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
+
