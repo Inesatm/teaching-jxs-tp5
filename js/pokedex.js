3a65a53dc832e30ad0b9244c5cdfa8e57992ec25
InesAtm <ineskatam@gmail.com>
2018-01-31 14:38:40 +0100

    pokemon's list

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,8 +1,18 @@
 var pokeApp = angular.module('pokedex', ['ngResource']);
 pokeApp.controller('searchPk', ['$scope', 
 function($scope) {
-    $scope.pokemon = {id: '', name: ''};
-    
+    $scope.pokemons =[
+            {name : 'Pikachu' , id : 1},
+            {name : 'Bulbizarre' , id : 2},
+            {name : 'Herbizarre' , id : 3},
+            {name : 'Aspicot' , id : 4},
+            {name : 'Ortide' , id : 5},
+            {name : 'Kabuto' , id : 6},
+            {name : 'Pyroli' , id : 7},
+            {name : 'Mew' , id : 8},
+            {name : 'Ronflex' , id : 9}
+    ];
+  
   }]);;
 // With this you can inject POKEAPI url wherever you want
 pokeApp.constant('POKEAPI', 'http://pokeapi.co');
