a3e2d6706fcee5b03ebbad8d1f40763f8a2e0068
Inesatm <ineskatam@gmail.com>
2018-03-20 04:32:18 +0100

    desciption & moves

--- js/pokedex.js
+++ js/pokedex.js
@@ -4,53 +4,55 @@ var pokeApp = angular.module('pokedex', ['ngResource']);
 // With this you can inject POKEAPI url wherever you want
 pokeApp.constant('POKEAPI', 'https://pokeapi.co/api/v2/');
 
-pokeApp.config(['$resourceProvider', function($resourceProvider) {
+pokeApp.config(['$resourceProvider', function ($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
-
-
-pokeApp.controller('searchPokemon',function($scope, $resource,PokedexService,POKEAPI){
-
+pokeApp.controller('searchPokemon', function ($scope, $resource, PokedexService, POKEAPI) {
     var ApiData = $resource(POKEAPI + "pokemon/?limit=60");
-    ApiData.get().$promise.then(function(results){
+    ApiData.get().$promise.then(function (results) {
         $scope.pokemons = results.results;
         console.log($scope.pokemons);
     });
-    $scope.ichooseu = function(pkmn){
-        PokedexService. getPokemon(pkmn);
-    }  }
-);
-pokeApp.factory('PokedexService', function($resource, $log, $rootScope){
+    $scope.ichooseu = function (pkmn) {
+        console.log("whoopsi " + pkmn);
+        PokedexService.getPokemon(pkmn);
+    }
+});
+pokeApp.factory('PokedexService', function ($resource, $log, $rootScope) {
     var pokemon = {};
-    var description = {};
-
-    function getPokemon(url){
-      
-        $resource(url).get().$promise.then(function(result){
+    var desc = {};
+    function getPokemon(url) {
+        var urlBase = $resource(url);
+        urlBase.get().$promise.then(function (result) {
             pokemon = result;
             //get the pokemon details(description text)
-            //var details = $resource(result.species.url);
-            $resource(result.species.url).get().$promise.then(function(result){
-             var description = result.flavor_text_entries;
-         // Get the latest English description.         
-                for (let i = 0; i < entries.length; i++) {
-                    const entry = entries[i];
-              if (entry.language.name === 'en') {
-                      description = entry.flavor_text;
-                      break;
-                    }
-                pokemon.description = description.flavor_text;
-            }});
+            var details = $resource(result.species.url);
+            details.get().$promise.then(function (result) {
+                var description = result.flavor_text_entries;
+                // Get the latest English description.         
+                for (i = 0; i < description.length; i++) {
+                    const entry = description[i];
+                    if (entry.language.name === 'en') {
+                        desc = entry.flavor_text;
+                        // console.log("sss"+desc);
+                        break;
+                                        }
+                }
+                pokemon.description = desc;
+                console.log("description" + pokemon.description);
+            });
         });
     }
-    function pokemon(){
+    function getPoke() {
         return pokemon;
     }
-    return {getPokemon: getPokemon, pokemon: pokemon}
+    return { getPokemon: getPokemon, getPoke: getPoke }
 });
-
-
-
-
-   
-    
+pokeApp.controller('pokeWtach', function ($scope, PokedexService) {
+    $scope.$watch(function () {
+        return PokedexService.getPoke()
+    },
+     function (newVal) {
+            $scope.pokemon = newVal;
+        });
+});
